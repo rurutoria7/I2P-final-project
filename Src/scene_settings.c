b209c7967b962c791f36511f0ae1429e3f60b87e
rurutoria7 <nandokabeam@gmail.com>
2023-01-13 19:15:18 +0800

    add exit button in setting scene

--- Src/scene_settings.c
+++ Src/scene_settings.c
@@ -3,7 +3,9 @@
 // you, so this 2 files is like the default scene template.
 #include "scene_settings.h"
 #include "scene_menu_object.h"
+#include "scene_menu.h"
 #include "game.h"
+#include "text_button.h"
 
 // Variables and functions with 'static' prefix at the top level of a
 // source file is only accessible in that file ("file scope", also
@@ -16,8 +18,9 @@ static Button music_vol_down_btn;
 static Button music_vol_up_btn;
 static Button effect_vol_down_btn;
 static Button effect_vol_up_btn;
-static int music_volume_percentage;
-static int effect_volume_percentage;
+static TextButton exit_btn;
+static int music_volume_percentage = 10;
+static int effect_volume_percentage = 10;
 
 static ALLEGRO_BITMAP *volume_block;
 
@@ -31,9 +34,11 @@ static void init(){
     music_vol_up_btn = button_create(SCREEN_W * 3.3 / 5, SCREEN_H * 1.0 / 5, 50, 50, "Assets/volume_up.png", "Assets/volume_up.png");
     effect_vol_down_btn = button_create(SCREEN_W * 1.05 / 5, SCREEN_H * 3.0 / 5, 50, 50, "Assets/volume_down.png", "Assets/volume_down.png");
     effect_vol_up_btn = button_create(SCREEN_W * 3.3 / 5, SCREEN_H * 3.0 / 5, 50, 50, "Assets/volume_up.png", "Assets/volume_up.png");
-    music_volume_percentage = 10;
-    effect_volume_percentage = 10;
     volume_block = load_bitmap("Assets/volume_block.png");
+
+
+
+    exit_btn = textButton_create(SCREEN_W * 4.0 / 5, SCREEN_H * 4.0 / 5, 100, 50, "EXIT", al_map_rgb(230, 230, 0));
 }
 
 static void draw(void ){
@@ -75,6 +80,7 @@ static void draw(void ){
                               0);
     }
 
+    drawTextButton(exit_btn);
 }
 
 static void on_mouse_move(int a, int mouse_x, int mouse_y, int f) {
@@ -85,6 +91,7 @@ static void on_mouse_move(int a, int mouse_x, int mouse_y, int f) {
     music_vol_up_btn.hovered = buttonHover(music_vol_up_btn, mouse_x, mouse_y);
     effect_vol_down_btn.hovered = buttonHover(effect_vol_down_btn, mouse_x, mouse_y);
     effect_vol_up_btn.hovered = buttonHover(effect_vol_up_btn, mouse_x, mouse_y);
+    exit_btn.hovered = textButtonHover(exit_btn, mouse_x, mouse_y);
 }
 
 static void on_mouse_down() {
@@ -113,6 +120,10 @@ static void on_mouse_down() {
     char c[100];
     sprintf(c, "music_volume = %d, effect_volume = %d", music_volume_percentage, effect_volume_percentage);
     game_log(c);
+
+    if (exit_btn.hovered){
+        game_change_scene(scene_menu_create());
+    }
 }
 
 // The only function that is shared across files.
